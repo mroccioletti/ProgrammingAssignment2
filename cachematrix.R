12c58a9acd83def29e123a7f2a477f8f4ddf3b11
Marco Roccioletti <mar@zweipunkt.local>
2017-07-18 10:57:09 +0200

    done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Peer Graded Assignment
+## Course: R Programming, Week: 3
+## Topic: Lexical Scoping
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates an object (list) to cache a matrix (data) and its inverse (inv)
 
+makeCacheMatrix <- function(m = matrix()) {
+
+  data <- NULL
+  inv <- NULL
+  
+  ## function to (re)set the cached matrix, its inverse is set to NULL (not computed yet)
+  set <- function(d) {
+    data <<- d
+    inv <<- NULL
+  }
+  ## function to get the cached matrix
+  get <- function() data
+  
+  ## function to cache the inverse
+  setinv <- function(i) inv <<- i
+  
+  ## function to get the cahced inverse
+  getinv <- function() inv
+  
+  ## set cahced matrix using the incoming parameter m
+  set(m)
+  
+  ## return the object
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## This function takes a parameter of type list representing
+## a cached matrix and its inverse created with the function 
+## makeCacheMatrix, calculates the inverse and caches the result 
+## within the list
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cm) {
+  
+  ## get inverse from cache object
+  inv <- cm$getinv()
+  
+  ## if NULL, compute it
+  if (is.null(inv)) {
+    
+    ## get data from cache object
+    message("Get data...")
+    data <- cm$get()
+    
+    ## compute inverse
+    message("Compute result...")
+    inv <-solve(data)
+
+    ## cache inverse in cache object
+    message("Cache result...")
+    cm$setinv(inv)
+    
+  }
+  
+  else {
+    ## do nothing if inv already cached (not NULL)
+    message("Result from cache!")
+  }
+  
+  ## return the inverse
+  inv
 }
